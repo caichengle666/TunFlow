b8deef0998ae02800665bead566dfd53d67c5285
caichengle666 <276177229+caichengle666@users.noreply.github.com>
2026-09-09 10:40:36 +0800

    feat(desktop): add non-Windows route manager stub

---
+++ desktop/platform_other.go
@@ -0,0 +1,18 @@
+//go:build !windows
+
+package main
+
+import "errors"
+
+type routeState struct {
+	active bool
+}
+
+func (a *App) setupRoutesLocked() error {
+	return errors.New("自动系统路由目前仅实现 Windows；可通过 TUN 参数自行配置其他系统")
+}
+
+func (a *App) teardownRoutesLocked() error {
+	a.up = routeState{}
+	return nil
+}
